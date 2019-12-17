a02313167bdee9d5bf7958e63d2196c33ab05549
isesame <isesame_live@163.com>
2019-12-18 01:02:46 +0800

    修复小问题

--- resources/views/Backend/articleCategoryList.blade.php
+++ resources/views/Backend/articleCategoryList.blade.php
@@ -67,7 +67,7 @@
                                         <div class="form-group">
                                             <label for="name" class="col-sm-2 control-label">分类名</label>
                                             <div class="col-sm-10">
-                                                <input type="text" class="form-control" name="name" id="new_name" placeholder="账号">
+                                                <input type="text" class="form-control" name="name" id="new_name" placeholder="分类名">
                                             </div>
                                         </div>
 
